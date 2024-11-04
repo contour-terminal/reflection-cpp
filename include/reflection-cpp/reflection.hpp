cc1968c76ffb897a410ffc794fae5ab2c86c9cbf
Christian Parpart <christian@parpart.family>
2024-11-04 21:34:58 +0100

    Fix clang-format off/on comments
    Signed-off-by: Christian Parpart <christian@parpart.family>

--- include/reflection-cpp/reflection.hpp
+++ include/reflection-cpp/reflection.hpp
@@ -202,7 +202,7 @@ inline constexpr decltype(auto) ToTuple(T&& t) noexcept
 {
     if constexpr (N == 0)
         return std::tuple {};
-    //clang-format off
+    // clang-format off
     else if constexpr (N == 1)
     {
         auto& [p0] = t;
@@ -453,7 +453,7 @@ inline constexpr decltype(auto) ToTuple(T&& t) noexcept
         auto& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49] = t;
         return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49);
     }
-    //clang-format on
+    // clang-format on
 }
 
 template <class T>
